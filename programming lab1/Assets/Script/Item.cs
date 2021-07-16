751f591177d62b567defd54f67552794b5ab90ed
murlord <ray12011994@gmail.com>
2021-07-15 22:41:30 -0400

    lab1

---
+++ programming lab1/Assets/Script/Item.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item 
+{
+    public enum ItemType
+    { 
+        SpeedPotion,
+        SizePotion,
+        ColorPotion,
+    }
+
+    public ItemType itemType;
+    public int amount;
+
+
+
+
+}
